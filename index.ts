c7143b7fe248484f61ee37b0684df128fdfac03c
rubentanahara <ruben.mtz.tanahara@outlook.com>
2025-06-20 15:29:29 -0700

    update: More context to model

--- index.ts
+++ index.ts
@@ -3,17 +3,22 @@ import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { fetchCityData, fetchWeather } from "./src/weatherhelper";
 
+// 1. MCP Server definition
 const server = new McpServer({
   name: "Demo",
   version: "1.0.0",
 });
 
+// 2. Tool definition
 server.tool(
   "fetch-weather",
+  "Get weather data for a city",
   {
-    title: "Weather Fetcher",
-    description: "Get weather data for a city",
-    inputSchema: { city: z.string().describe("Name of the city") },
+    city: z
+      .string()
+      .describe(
+        "The name of the city for which the weather data is requested. This should be a valid city name recognized by the location service.",
+      ),
   },
   async ({ city }) => {
     const cityData = await fetchCityData(city);
@@ -23,7 +28,7 @@ server.tool(
         content: [
           {
             type: "text",
-            text: `No location found or failed to fetch location data for ${city}.`,
+            text: `No location found or failed to fetch location data for ${city}. Please check the city name and try again.`,
           },
         ],
       };
@@ -39,11 +44,12 @@ server.tool(
         content: [
           {
             type: "text",
-            text: `Failed to fetch weather data for ${city}.`,
+            text: `Failed to fetch weather data for ${city}. Please try again later or check your network connection.`,
           },
         ],
       };
     }
+
     // The LLM will interpret the data
     return {
       content: [
@@ -56,5 +62,7 @@ server.tool(
   },
 );
 
+// 3. Connection via transoprt
+// since is local we use stdio
 const transport = new StdioServerTransport();
 await server.connect(transport);
